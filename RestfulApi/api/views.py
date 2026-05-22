a63fba295b3636660c34c57a69b5d147509cac5f
Sahan <sahanshrestha2000@gmail.com>
2026-05-22 13:50:41 +0545

    serializers

--- RestfulApi/api/views.py
+++ RestfulApi/api/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from students.models import Student
+from .searializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+
+
+@api_view(['GET'])
 def studentViews(request):
-    students = Student.objects.all()
-    students = list(students.values())
-    return JsonResponse(students, safe=False)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
