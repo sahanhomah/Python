9ec2780167fffd950580b610aa63c400a725ed5d
Sahan <sahanshrestha2000@gmail.com>
2026-06-12 11:54:53 +0545

    Data initializattion in database

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from models import Products
 from database import session,engine
 import database_models
+from pydantic import BaseModel
 app = FastAPI()
 database_models.Base.metadata.create_all(bind=engine)
 
@@ -9,6 +10,27 @@ products = [
     {"id": 1, "name": "Laptop", "price": 999.99, "description": "A high-performance laptop for work and play.", "quantity": 10},
     {"id": 2, "name": "Smartphone", "price": 499.99, "description": "A sleek smartphone with a powerful camera.", "quantity": 20},
 ]
+
+def init_db():
+    db = session()
+
+    count =db.query(database_models.Products).count
+
+    if count==0: 
+
+         for product in products:
+          db.add(database_models.Products(**product))
+  
+
+
+
+         db.commit()
+    db.close()
+
+init_db()    
+
+
+
 @app.get("/")
 def greet():
     return "Hello, and welcome to this !"
@@ -19,15 +41,13 @@ def get_products():
 
 @app.get("/products/{id}")
 def get_product(id: int):
-
-    db=session()
-    db.query()
     for product in products:
-        if product[i]["id"] == id:
+        if product["id"] == id:
             return product
     
     
     return " Product not found"
+
 @app.post("/products")
 def create_product(product: Products):
     products.append(product.model_dump())
